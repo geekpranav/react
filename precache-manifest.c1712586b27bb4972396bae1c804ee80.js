da7a13a7036f8a9b9d0a478fe10642e2c021d02a
pranav shankar <candypranav@gmail.com>
2020-07-14 21:28:15 +0530

    Updates

---
+++ precache-manifest.c1712586b27bb4972396bae1c804ee80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0494c03da872f3dc77698a067fab30",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "5c230ba4c834f359f9e9",
+    "url": "/react/static/css/main.ca15f251.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/react/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c230ba4c834f359f9e9",
+    "url": "/react/static/js/main.7146d64f.chunk.js"
+  },
+  {
+    "revision": "e16830573a61ddcfcf9f",
+    "url": "/react/static/js/runtime-main.54f9499d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
